e6bf6765cbd9b753971712d579035c2deea66961
sjhuang <hsjfans_scholar@163.com>
2021-10-11 16:45:32 +0800

    fix layer

--- train.py
+++ train.py
@@ -13,17 +13,49 @@ import config
 from ctc import  ctc_decode
 from  tqdm import  tqdm
 
-def evaluate(crnn, dataloader, criterion,device,
-             max_iter=None, decode_method='beam_search', beam_size=10):
+
+def train(epoch,show_interval,crnn, optimizer, criterion, device,train_loader):
+    """
+
+    :param epoch:
+    :param show_interval:
+    :param crnn:
+    :param optimizer:
+    :param criterion:
+    :param device:
+    :param train_loader:
+    :return:
+    """
+    tot_train_loss = 0.
+    tot_train_count = 0
+    for train_data in train_loader:
+        crnn.train()
+        images, targets, target_lengths = [d.to(device) for d in train_data]
+        logits = crnn(images)
+        log_probs = F.log_softmax(logits, dim=2)
+        batch_size = images.size(0)
+        input_lengths = torch.LongTensor([logits.size(0)] * batch_size)
+        target_lengths = torch.flatten(target_lengths)
+        loss = criterion(log_probs, targets, input_lengths, target_lengths)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        train_size = train_data[0].size(0)
+        tot_train_loss += loss.item()
+        tot_train_count += train_size
+    # if epoch % show_interval == 0:
+        logger.info(f'Train epoch :{epoch}, train_loss: {tot_train_loss / tot_train_count}')
+
+
+def valid(epoch,model_name,crnn, criterion, device, dataloader,val_loss,early_num,checkpoints_dir,
+          max_iter=None, decode_method='beam_search', beam_size=10):
     crnn.eval()
     tot_count = 0
     tot_loss = 0
     tot_correct = 0
     wrong_cases = []
-
     pbar_total = max_iter if max_iter else len(dataloader)
     pbar = tqdm(total=pbar_total, desc="Evaluate")
-
     with torch.no_grad():
         for i, data in enumerate(dataloader):
             if max_iter and i >= max_iter:
@@ -36,7 +68,6 @@ def evaluate(crnn, dataloader, criterion,device,
             loss = criterion(log_probs, targets, input_lengths, target_lengths)
             preds = ctc_decode(log_probs, method=decode_method, beam_size=beam_size)
             reals = targets.cpu().numpy().tolist()
-            #print(len(reals),len(preds),input_lengths)
             target_lengths = target_lengths.cpu().numpy().tolist()
             tot_count += batch
             tot_loss += loss.item()
@@ -44,38 +75,26 @@ def evaluate(crnn, dataloader, criterion,device,
             for pred, target_length in zip(preds, target_lengths):
                 real = reals[target_length_counter:target_length_counter + target_length]
                 target_length_counter += target_length
-
-                if len(pred) >= len(real) and real == pred[:len(real)]:
+                if real == pred:
                     tot_correct += 1
                 else:
                     wrong_cases.append((real, pred))
             pbar.update(1)
         pbar.close()
 
-    evaluation = {
-        'loss': tot_loss / tot_count,
-        'acc': tot_correct / tot_count,
-        'wrong_cases': wrong_cases
-    }
-    logger.debug(wrong_cases)
-    return evaluation
-
-
-def train_batch(crnn, data, optimizer, criterion, device):
-    crnn.train()
-    images, targets, target_lengths = [d.to(device) for d in data]
-    logits = crnn(images)
-    log_probs = F.log_softmax(logits, dim=2)
-    batch_size = images.size(0)
-    input_lengths = torch.LongTensor([logits.size(0)] * batch_size)
-    target_lengths = torch.flatten(target_lengths)
-
-    loss = criterion(log_probs, targets, input_lengths, target_lengths)
-
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    return loss.item()
+    valid_loss = tot_loss / tot_count
+    valid_acc = tot_correct / tot_count
+    logger.info(f'Valid epoch:{epoch}, loss:{valid_loss}, acc:{valid_acc}')
+    if val_loss > valid_loss:
+        val_loss = valid_loss
+        early_num = 0
+        save_model_path = os.path.join(checkpoints_dir,
+                                       f'{model_name}.pt')
+        torch.save(crnn.state_dict(), save_model_path)
+        logger.info(f'save model at {save_model_path}')
+    else:
+        early_num += 1
+    return val_loss,early_num
 
 
 def main(train_data_path,goto_train):
@@ -106,40 +125,17 @@ def main(train_data_path,goto_train):
     early_num = 0
     val_loss = (1 << 10)
     for epoch in tqdm(range(1, epochs + 1)):
-        tot_train_loss = 0.
-        tot_train_count = 0
-        for train_data in t_loader:
-            loss = train_batch(crnn, train_data, optimizer, criterion, device)
-            train_size = train_data[0].size(0)
-            tot_train_loss += loss
-            tot_train_count += train_size
-
-        if epoch % show_interval == 0:
-            logger.info(f'Train epoch :{epoch}, train_loss: {tot_train_loss / tot_train_count}')
-        if epoch % valid_interval == 0:
-            evaluation = evaluate(crnn, valid_loader, criterion,device,
-                                  decode_method=crc_train_config['decode_method'],
+        train(epoch,show_interval,crnn,optimizer,criterion,device,t_loader)
+        val_loss, early_num = valid(epoch,model_name,crnn, criterion, device, valid_loader,val_loss,early_num,checkpoints_dir,
+                           decode_method=crc_train_config['decode_method'],
                                   beam_size=crc_train_config['beam_size'])
-            valid_loss = evaluation['loss']
-            valid_acc = evaluation['acc']
-            logger.info(f'Valid epoch:{epoch}, loss:{valid_loss}, acc:{valid_acc}')
-            if val_loss > valid_loss:
-                val_loss = valid_loss
-                early_num = 0
-                save_model_path = os.path.join(checkpoints_dir,
-                                               f'{model_name}_{epoch}_loss{valid_loss}.pt')
-                torch.save(crnn.state_dict(), save_model_path)
-                logger.info(f'save model at {save_model_path}')
-            else:
-                early_num += 1
-            if early_num > early_stop:
-                logger.info(f"Early Stop in epoch:{epoch}")
-                break
-
+        if early_num > early_stop:
+            logger.info(f"Early Stop in epoch:{epoch}")
+            break
 
 if __name__ == '__main__':
     import argparse
-    seed = 10
+    seed = 100
     setup_seed(seed)
     parser = argparse.ArgumentParser(description='Train captcha model')
     parser.add_argument('--train_path', type=str,required= False,default= config.train_data_path, help='The path of train dataset')
